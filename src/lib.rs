0b7c0dd71e1e8a88a7e46dc307429fa20c9d96c6
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-02-06 01:25:15 +0000

    Updated documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 MaidSafe.net limited.
+// Copyright 2016 MaidSafe.net limited.
 //
 // This SAFE Network Software is licensed to you under (1) the MaidSafe.net Commercial License,
 // version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
@@ -17,48 +17,44 @@
 
 //! # Secure Serialisation
 //!
-//! Given a remote nacl box PublicKey this lib will securely serialise and
-//! encrypt messages destined for that node. These will use authenticated encryption.
+//! Given a remote nacl box PublicKey this lib will securely serialise and encrypt messages destined
+//! for that node.  These will use authenticated encryption.
 //!
 //! # Authenticated encryption
-//! Using public-key authenticated encryption, Bob can encrypt a confidential message specificall
-//! for Alice, using Alice's public key.+
+//! Using public-key authenticated encryption, Bob can encrypt a confidential message specifically
+//! for Alice, using Alice's public key.
 //!
 //! Using Bob's public key, Alice can verify that the encrypted message was actually created by Bob
 //! and was not tampered with, before eventually decrypting it.
-//! Alice only needs Bob's public key, the nonce and the ciphertext. Bob should never ever share his
-//! secret key, even with Alice.
-//! And in order to send messages to Alice, Bob only needs Alice's public key. Alice should never
-//! ever share her secret key either, even with Bob.
+//!
+//! Alice only needs Bob's public key, the nonce and the ciphertext.  Bob should never ever share
+//! his secret key, even with Alice.  And in order to send messages to Alice, Bob only needs Alice's
+//! public key.  Alice should never ever share her secret key either, even with Bob.
+//!
 //! Alice can reply to Bob using the same system, without having to generate a distinct key pair.
 //! The nonce doesn't have to be confidential, but it should be used with just one invocation of
-//! crypto_box_open_easy() for a particular pair of public and secret keys.
+//! `crypto_box_open_easy()` for a particular pair of public and secret keys.
 //!
-//! One easy way to generate a nonce is to use randombytes_buf(), considering the size of nonces
-//! the risk of any random collisions is negligible. For some applications, if you wish to use
-//! nonces to detect missing messages or to ignore replayed messages, it is also ok
-//! to use a simple incrementing counter as a nonce. In this crate we use a random nonce wrapped
-//! into the message.
+//! One easy way to generate a nonce is to use `randombytes_buf()`; considering the size of nonces
+//! the risk of any random collisions is negligible.  For some applications, if you wish to use
+//! nonces to detect missing messages or to ignore replayed messages, it is also OK to use a simple
+//! incrementing counter as a nonce.  In this crate we use a random nonce wrapped into the message.
 //!
-//! This implementation will encrypt data with a nonce and then serialise the payload. The nonce
-//! is then prepended to the beginning of the message and pulled off first at the remote end.
-//! This provides a clean secure mechanism for sending data between entities who have session
-//! based keypairs. It SHOULD NOT be used for permanent keys.
+//! This implementation will encrypt data with a nonce and then serialise the payload.  The nonce
+//! is then prepended to the message and pulled off first at the remote end.  This provides a clean,
+//! secure mechanism for sending data between entities who have session-based keypairs.  It SHOULD
+//! NOT be used for permanent keys.
 //!
-//! Where possible the precompute_* functions will lessen any cpu overhead in sending messages
-//! and should be preferred. This is not enforced to allow occasional sending of messages
-//! between parties using a simpler, although slower, method.
+//! Where possible the `precompute_*` functions will lessen any CPU overhead in sending messages and
+//! should be preferred.  This is not enforced to allow occasional sending of messages between
+//! parties using a simpler, although slower, method.
 //!
-//! These functions do not meant to provide non-repudiation. On the
-//! contrary: they  guarantee repudiability. A receiver
-//! can freely modify a message, and therefore cannot convince third
-//! parties that this particular message came from the sender. The sender
-//! and receiver are nevertheless protected against forgeries by other
-//! parties. In the terminology of
-//! http://groups.google.com/group/sci.crypt/msg/ec5c18b23b11d82c,
-//! this crate uses "public-key authenticators" rather than "public-key
-//! signatures."
-
+//! These functions are not meant to provide non-repudiation.  On the contrary: they  guarantee
+//! repudiability.  A receiver can freely modify a message, and therefore cannot convince third
+//! parties that this particular message came from the sender.  The sender and receiver are
+//! nevertheless protected against forgeries by other parties.  In the terminology of
+//! http://groups.google.com/group/sci.crypt/msg/ec5c18b23b11d82c, this crate uses "public-key
+//! authenticators" rather than "public-key signatures."
 
 #![doc(html_logo_url =
            "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
@@ -70,37 +66,37 @@
 #![forbid(bad_style, exceeding_bitshifts, mutable_transmutes, no_mangle_const_items,
           unknown_crate_types, warnings)]
 #![deny(deprecated, drop_with_repr_extern, improper_ctypes, missing_docs,
-      non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
-      private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
-      unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
-      unused_comparisons, unused_features, unused_parens, while_true)]
+        non_shorthand_field_patterns, overflowing_literals, plugin_as_library,
+        private_no_mangle_fns, private_no_mangle_statics, stable_features, unconditional_recursion,
+        unknown_lints, unsafe_code, unused, unused_allocation, unused_attributes,
+        unused_comparisons, unused_features, unused_parens, while_true)]
 #![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
-        unused_qualifications, unused_results, variant_size_differences)]
+        unused_qualifications, unused_results)]
 #![allow(box_pointers, fat_ptr_transmutes, missing_copy_implementations,
-         missing_debug_implementations)]
+         missing_debug_implementations, variant_size_differences)]
 
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
-#![cfg_attr(feature="clippy", deny(clippy))]
-#![cfg_attr(feature="clippy", deny(clippy_pedantic))]
+#![cfg_attr(feature="clippy", deny(clippy, clippy_pedantic))]
 
-extern crate sodiumoxide;
 #[macro_use]
 extern crate maidsafe_utilities;
 extern crate rustc_serialize;
+extern crate sodiumoxide;
+
+pub use sodiumoxide::crypto::box_::{PrecomputedKey, PublicKey, SecretKey, gen_keypair};
 
 use sodiumoxide::crypto::box_;
-pub use sodiumoxide::crypto::box_::{PrecomputedKey, PublicKey, SecretKey, gen_keypair};
 use maidsafe_utilities::serialisation;
 use rustc_serialize::{Decodable, Encodable};
 
-/// Error types, hopefully sodiumoxide eventually defines errors properly, otherwise this makes
-/// little sense really
+/// Error types.
+///
+/// Hopefully sodiumoxide eventually defines errors properly, otherwise this makes little sense.
+#[allow(missing_docs)]
 #[derive(Debug)]
 pub enum SecureSerialisationError {
-    /// error form serialisation crate
     SerialisationError(serialisation::SerialisationError),
-    /// unfortunately sodiumoxide uses () as error types ?
     CrytpoError,
 }
 
@@ -116,7 +112,7 @@ impl From<()> for SecureSerialisationError {
     }
 }
 
-#[derive (RustcEncodable, RustcDecodable)]
+#[derive(RustcEncodable, RustcDecodable)]
 struct Payload {
     ciphertext: Vec<u8>,
     nonce: box_::Nonce,
@@ -127,9 +123,9 @@ pub fn pre_compute(their_public_key: &PublicKey, our_secret_key: &SecretKey) -> 
     box_::precompute(their_public_key, our_secret_key)
 }
 
-/// Pre computed key version (less cpu intensive if many messages are transferred)
-/// Prepare an encodable data element for transmission to another process whose public_key we
-/// know.
+/// Prepare an encodable data element for transmission to another process whose public key we
+/// know, and which is pre-computed.  This is less CPU-intensive than
+/// [`serialise()`](fn.serialise.html) which can be useful if many messages are to be transferred.
 pub fn pre_computed_serialise<T>(data: &T,
                                  pre_computed_key: &PrecomputedKey)
                                  -> Result<Vec<u8>, SecureSerialisationError>
@@ -146,8 +142,7 @@ pub fn pre_computed_serialise<T>(data: &T,
 }
 
 
-/// Prepare an encodable data element for transmission to another process whose public_key we
-/// know.
+/// Prepare an encodable data element for transmission to another process whose public key we know.
 pub fn serialise<T>(data: &T,
                     their_public_key: &PublicKey,
                     our_secret_key: &SecretKey)
@@ -164,9 +159,10 @@ pub fn serialise<T>(data: &T,
     Ok(try!(serialisation::serialise(&full_payload)))
 }
 
-/// Pre computed key version (less cpu intensive if many messages are transferred)
-/// Parse a data type from an encoded message, success ensures the message was from the holder of the
-/// private_key related to the public_key we know of the recipient
+/// Parse a data type from an encoded message from a sender whose public key we know, and which is
+/// pre-computed.  This is less CPU-intensive than [`deserialise()`](fn.deserialise.html) which can
+/// be useful if many messages are to be transferred.  Success ensures the message was from the
+/// holder of the private key related to the public key we know of the sender.
 pub fn pre_computed_deserialise<T>(message: &[u8],
                                    pre_computed_key: &PrecomputedKey)
                                    -> Result<T, SecureSerialisationError>
@@ -179,8 +175,9 @@ pub fn pre_computed_deserialise<T>(message: &[u8],
     Ok(try!(serialisation::deserialise(&plain_serialised_data)))
 }
 
-/// Parse a data type from an encoded message, success ensures the message was from the holder of the
-/// private_key related to the public_key we know of the recipient
+/// Parse a data type from an encoded message from a sender whose public key we know.  Success
+/// ensures the message was from the holder of the private key related to the public key we know of
+/// the sender.
 pub fn deserialise<T>(message: &[u8],
                       their_public_key: &PublicKey,
                       our_secret_key: &SecretKey)
